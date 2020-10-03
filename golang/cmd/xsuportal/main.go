c1ac98b4ecacf518e1bdc999008980b484b52b7d
sapphi-red <green@sapphi.red>
2020-10-03 17:52:09 +0900

    decrease load

--- golang/cmd/xsuportal/main.go
+++ golang/cmd/xsuportal/main.go
@@ -40,7 +40,7 @@ import (
 
 const (
 	// TODO: これをあげることで負荷をあげられる、300でボーナス倍率が2倍になる
-	TeamCapacity               = 77
+	TeamCapacity               = 75
 	AdminID                    = "admin"
 	AdminPassword              = "admin"
 	DebugContestStatusFilePath = "/tmp/XSUPORTAL_CONTEST_STATUS"
